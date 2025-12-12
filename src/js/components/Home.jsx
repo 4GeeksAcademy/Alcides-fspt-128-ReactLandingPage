f9f01ac9277ec2f066e36925aaa8022a876ab1ea
Alcides Rivera <elarsi626@gmail.com>
2025-12-12 20:30:16 +0000

    Primero

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -2,26 +2,26 @@ import React from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
+import Navbar from "./Navbar";
+import Carousel from "./Carousel";
 
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-            
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+	const videojuegos = [{
+		id : 1 
+		nombre: "Black Desert online",
+		plataforma: "PC, PS4, XBOX"
+		imagen:
+	}];
+
+
+	return (
+		<>
+		<Navbar />
+		<Carousel />
+		<div></div>
+		</>
 	);
 };
 
